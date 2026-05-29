f80a1feb02746bf5214808dba9d32c39f324ed5b
praster1 <praster1@gmail.com>
2026-05-29 23:49:39 +0900

    Add admin members context column snapshot

---
+++ scripts_go/web_r_go_20260529_2330/scripts/common/board/read/comment/Div_btn_comment_editor_footer_button.js
@@ -0,0 +1,11 @@
+function Div_btn_comment_editor_footer_button(props) {
+  return /* @__PURE__ */ React.createElement(
+    "button",
+    {
+      type: "button",
+      onClick: props.function,
+      class: "flex flex-row justify-center items-center\n					   text-white bg-gradient-to-r from-cyan-500 to-blue-500 font-medium rounded-lg text-sm px-5 py-1 text-center\n					   hover:bg-gradient-to-bl hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-cyan-300"
+    },
+    "\uB4F1\uB85D"
+  );
+}
