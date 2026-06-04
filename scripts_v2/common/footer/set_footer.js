2c05fb60c15180c42427331807cc178571456c62
praster1 <praster1@gmail.com>
2026-06-04 11:32:27 +0900

    Fix Web-R footer business info

--- scripts_v2/common/footer/set_footer.js
+++ scripts_v2/common/footer/set_footer.js
@@ -2,8 +2,8 @@ let data_footer = {
 	"company": "주식회사 통계마당",
 	"representative": "대표, 개인정보보호책임자: 유재성",
 	"registration_no": "사업자등록번호: 795-88-02574",
-	"mail_order_no": "통신판매업신고번호: 2024-서울동작-0216",
-	"address": "서울특별시 동작구 동작대로7길 48, 새한빌딩 502호",
+	"mail_order_no": "통신판매업신고번호: 2024-서울강남-06145",
+	"address": "서울특별시 강남구 테헤란로70길 12, 402-106A호",
 	"phone": "대표전화: 0507-1300-9704",
 	"email": "문의: cs@statground.net",
 }
